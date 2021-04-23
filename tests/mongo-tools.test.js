49d067ab5f17e189fc5970f0f4b75a067719d006
Brice Vandeputte <brice.vandeputte@orange.com>
2021-04-23 14:05:43 +0200

    Fix #18 improve wrapper command, add more tests

--- tests/mongo-tools.test.js
+++ tests/mongo-tools.test.js
@@ -14,6 +14,7 @@ const testPort = process.env.MT_MONGO_PORT || 27017;
 var mt = null;
 var mtOptions = null;
 var lastDumpFile = null;
+var nbBackupExpected = 0;
 
 function logOutput(result) {
   if (result.stdout) { console.info('stdout:', result.stdout); }
@@ -21,7 +22,11 @@ function logOutput(result) {
 }
 function logSuccess(success) {
   logOutput(success);
-  console.info(`OK ${success.message}`);
+  if (success.message) {
+    console.info(`OK ${success.message}`);
+    return;
+  }
+  console.info(`OK ${JSON.stringify(success, null, 4)}`);
 }
 
 describe("Mongo Tools", function() {
@@ -29,11 +34,13 @@ describe("Mongo Tools", function() {
     before(async function () {
       console.info("Mongo Tools :: before");
       fs.rmdirSync(testBackupDirectory, { recursive: true });
+      nbBackupExpected = 0;
       mt = new MongoTools();
       mtOptions = new MTOptions({
         db: testDbName,
         port: testPort,
         path: testBackupDirectory,
+        fileName: 'should_dump_db_locally.gz',
         dropboxToken: null,
         showCommand: true
       });
@@ -47,6 +54,7 @@ describe("Mongo Tools", function() {
       dumpResult.fileName.should.not.be.eql(null);
       dumpResult.fullFileName.should.not.be.eql(null);
       lastDumpFile = dumpResult.fullFileName;
+      nbBackupExpected++;
     });
 
     it("should dump database from uri", async function() {
@@ -61,6 +69,7 @@ describe("Mongo Tools", function() {
       dumpResult.fullFileName.should.not.be.eql(null);
       dumpResult.fullFileName.should.startWith('tests/backup/myDbForTest__2');
       lastDumpFile = dumpResult.fullFileName;
+      nbBackupExpected++;
     });
 
     it("should restore database", async function() {
@@ -71,6 +80,54 @@ describe("Mongo Tools", function() {
       restoreResult.status.should.be.eql(0);
     });
 
+    it("should list backup", async function() {
+      const listResult = await mt.list(mtOptions).catch(_expectNoError);
+      logSuccess(listResult);
+      expect(listResult.filesystem).to.have.lengthOf(nbBackupExpected);
+      expect(listResult.filesystem).to.include(lastDumpFile)
+      listResult.path.should.be.eql(testBackupDirectory);
+    });
+
+    it("should dry rotate backups", async function() {
+      const rotateResult = await mt.rotation({
+        path: testBackupDirectory,
+        dropboxToken: null,
+        rotationWindowsDays: 0,
+        rotationMinCount:0,
+        rotationDryMode: true
+      }).catch(_expectNoError);
+      logSuccess(rotateResult);
+      rotateResult.filesystem.initialBackupsCount.should.be.eql(nbBackupExpected);
+      rotateResult.filesystem.deprecatedBackupsCount.should.be.eql(nbBackupExpected);
+      rotateResult.filesystem.cleanedCount.should.be.eql(nbBackupExpected);
+    });
+
+    it("should rotate backups", async function() {
+      const rotateResult = await mt.rotation({
+        path: testBackupDirectory,
+        dropboxToken: null,
+        rotationWindowsDays: 0,
+        rotationMinCount:0
+      }).catch(_expectNoError);
+      logSuccess(rotateResult);
+      rotateResult.filesystem.initialBackupsCount.should.be.eql(nbBackupExpected);
+      rotateResult.filesystem.deprecatedBackupsCount.should.be.eql(nbBackupExpected);
+      rotateResult.filesystem.cleanedCount.should.be.eql(nbBackupExpected);
+    });
+
+    it("should rotate no backups", async function() {
+      const rotateResult = await mt.rotation({
+        path: testBackupDirectory,
+        dropboxToken: null,
+        rotationWindowsDays: 0,
+        rotationMinCount:0,
+        rotationDryMode: true
+      }).catch(_expectNoError);
+      logSuccess(rotateResult);
+      rotateResult.filesystem.initialBackupsCount.should.be.eql(0);
+      rotateResult.filesystem.deprecatedBackupsCount.should.be.eql(0);
+      rotateResult.filesystem.cleanedCount.should.be.eql(0);
+    });
 });
 
 
