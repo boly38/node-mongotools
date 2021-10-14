495dd90aef950dfdea67254fc605ba0ad45b7e57
Brice Vandeputte <brice.vandeputte@orange.com>
2021-10-14 22:19:11 +0200

    Fix #26 add encrypt/decrypt backup

--- env/initEnv.template.sh
+++ env/initEnv.template.sh
@@ -3,7 +3,12 @@ export MT_MONGO_DB=myDatabase
 export MT_MONGO_PORT=37017
 export MT_MONGO_USER=root
 export MT_MONGO_PWD=mypass
+export MT_MONGO_AUTH_DB=admin
 
 # optional dropbox feature
 # https://www.dropbox.com/developers/apps/
 export MT_DROPBOX_TOKEN=
+
+export MT_PATH=backup
+# MT_SECRET: used to encrypt backup - size 32 - change it
+# export MT_SECRET=12345678901234567890123456789012
