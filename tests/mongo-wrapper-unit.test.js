b3034f1a450eb64f528b47cec0f85b98f1bd5cf3
Brice Vandeputte <brice.vandeputte@orange.com>
2022-10-23 14:24:44 +0200

    Fix #54 outdated deps, switch to module type

--- tests/mongo-wrapper-unit.test.js
+++ tests/mongo-wrapper-unit.test.js
@@ -1,11 +1,14 @@
-const MTWrapper = require("../lib/MTWrapper");
-const MTOptions = require("../lib/MTOptions");
+import MongoTools from "../lib/MongoTools.js";
+import MTOptions from "../lib/MTOptions.js";
+import MTWrapper from "../lib/MTWrapper.js";
 
-const chai = require('chai');
-const assert = require('assert').strict;
-const expect = chai.expect
+import chai from 'chai';
+import chaiString from 'chai-string';
+const should = chai.should;
+const expect = chai.expect;
+import { strict as assert } from 'assert';
 chai.should();
-chai.use(require('chai-string'));
+chai.use(chaiString);
 
 const testDbUsername = process.env.MT_MONGO_USER || null;
 const testDbPassword = process.env.MT_MONGO_PWD || null;
