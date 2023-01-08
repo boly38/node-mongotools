b3341e31bdb373617972edf22ee7cdfad2350ba5
Brice Vandeputte <brice.vandeputte@orange.com>
2023-01-08 15:05:28 +0100

    Fix #68 dump and restore a collection

--- mt.js
+++ mt.js
@@ -1,4 +1,5 @@
-const {MTCommand} = require('./lib/mt.js');
+import { MTCommand } from './lib/mt.js'; // as library client you may use : import { MongoTools, MTOptions, MTCommand } from "node-mongotools";
+
 // take first command line argument
 var action = process.argv.slice(2)[0];
 (new MTCommand()).doAction(action);
