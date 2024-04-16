7cf972a811479c2e27806ae30393da91fae57c4d
Brice Vandeputte <brice.vandeputte@orange.com>
2024-04-16 14:17:55 +0200

    Fix #79 add dropbox-refresh-token
    - add `dropboxAppKey`, `dropboxAppSecret` and `dropboxRefreshToken` (long-lived offline refresh token) : required to activate optional dropbox feature.
    - MT_DROPBOX_TOKEN is now deprecated
    - refacto about Promise
    
    reject

--- env/initEnv.template.sh
+++ env/initEnv.template.sh
@@ -5,10 +5,17 @@ export MT_MONGO_USER=root
 export MT_MONGO_PWD=mypass
 export MT_MONGO_AUTH_DB=admin
 
-# optional dropbox feature
-# https://www.dropbox.com/developers/apps/
-export MT_DROPBOX_TOKEN=
-
 export MT_PATH=backup
 # MT_SECRET: used to encrypt backup - size 32 - change it
 # export MT_SECRET=12345678901234567890123456789012
+
+# TIP: get key,secret from dropbox developers app dev portal : https://www.dropbox.com/developers/apps/
+export MT_DROPBOX_APP_KEY=
+export MT_DROPBOX_APP_SECRET=
+# TIP: long-lived offline refresh-token. cf. https://github.com/boly38/dropbox-refresh-token
+export MT_DROPBOX_REFRESH_TOKEN=
+
+## DEPRECATED SECTION
+# MT_DROPBOX_TOKEN - old-long-lived access-token - no more available from dropbox developers portal
+# export MT_DROPBOX_TOKEN=
+## DEPRECATED SECTION end
